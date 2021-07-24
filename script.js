5bbb1d60385562107e119c2badb17593c6f3158c
JEETH-SIRIL-NUTHLAPATI <jsnpaati@gmail.com>
2021-07-24 12:15:42 +0530

    Changing styles using DOM

--- script.js
+++ script.js
@@ -16,6 +16,10 @@ document.querySelector('.check').addEventListener
     } 
     else if (guess === secretNumber){
        document.querySelector('.message').textContent = '🎇 Correct Number!';
+
+       document.querySelector('body').style.backgroundColor = '#60b347';
+
+       document.querySelector('.number').style.width = '30rem';
     }
     else if (guess > secretNumber){
          if(score > 1){
