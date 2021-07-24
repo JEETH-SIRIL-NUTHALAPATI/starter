3364714e463b318f565301fc5d6bec38f5a6d676
JEETH-SIRIL-NUTHLAPATI <jsnpaati@gmail.com>
2021-07-24 11:46:21 +0530

    Comparing entered Number with generated number.

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-'use strict';
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+
+document.querySelector('.check').addEventListener
+('click',function() {
+    const guess = Number(document.querySelector('.guess').value);
+
+    console.log(typeof guess);
+
+    if(!guess){
+        document.querySelector('.message').textContent = '👿 No NUmber!';
+    } 
+    else if (guess === secretNumber){
+       document.querySelector('.message').textContent = '🎇 Correct Number!';
+    }
+    else if (guess > secretNumber){
+        document.querySelector('.message').textContent = ' Too High!';
+    }
+    else if (guess < secretNumber){
+        document.querySelector('.message').textContent = ' Too Low!';
+    }
+});
 
